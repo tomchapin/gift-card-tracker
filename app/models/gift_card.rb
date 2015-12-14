c3c45447fe64723a5de2d4e35eaebd09631657ec
Tom Chapin <tchapin@gmail.com>
2015-12-13 21:00:30 -0700

    Monetizing money columns on models

--- app/models/gift_card.rb
+++ app/models/gift_card.rb
@@ -1,4 +1,7 @@
 class GiftCard < ActiveRecord::Base
   belongs_to :card_issuer
   has_many :transactions
+
+  monetize :balance_cents
+
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,6 @@
 class Transaction < ActiveRecord::Base
   belongs_to :gift_card
+
+  monetize :amount_cents
+
 end
