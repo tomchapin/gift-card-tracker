b02c63aa8c6a3f1e76116d0980b1597dbdc26b75
Tom Chapin <tchapin@gmail.com>
2015-12-14 21:23:44 -0700

    Adding test coverage calculator, simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,12 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+# Enable SimpleCov
+require 'simplecov'
+SimpleCov.start
+
+# Rspec configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
